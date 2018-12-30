3675aebd5b99d4f5776c0faaf60022d9e9eef1fd
stteffano13 <tefo.aguayo@gmail.com>
2018-12-30 15:08:43 -0500

    Interfaz pagina proincipal terminada

--- kansapp/src/pages/nueva_oferta/nueva_oferta.ts
+++ kansapp/src/pages/nueva_oferta/nueva_oferta.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
-import { Platform, Nav,  } from 'ionic-angular';
+import { Platform, Nav, } from 'ionic-angular';
 import { NavController, AlertController } from "ionic-angular";
 import { UserService } from "../../app/services/user.services";
 import { PublicistaPage } from '../publicista/publicista';
@@ -12,30 +12,45 @@ import { Observable } from "rxjs";
 })
 
 export class NuevaOfertaPage {
- 
-    public Categoria;
-    public DescripcionNuevaCategoria;
 
-    public bander = false;
-    
-    public isActiveToggleTextPassword=false;
-    
-    public categoriaV: any[] = [{ "categoria": "Educacion" },
-    { "categoria": "Ciencia" },
-    { "categoria": "Tecnologia" },
-    { "categoria": "Arte" },
-    { "categoria": "Cultura" },
-    { "categoria": "Gastronomia" },
-    { "categoria": "Medicina" },
-    { "categoria": "Otro" },
-];
+  public Categoria;
+  public DescripcionNuevaCategoria;
+
+  public tiempoO;
+  public precioO;
+  public alcanceO;
+  public inf_adicional;
+
+  public bander = false;
+  public banderOtro = false;
+
+  //Activar botones de redes sociales
+  public btnFaceboockAct = true;
+  public btnFaceboock = false;
+
+  public btnInstagramAct = true;
+  public btnInstagram = false;
+
+  public btnTwiterAct = true;
+  public btnTwiter = false;
+  public isActiveToggleTextPassword = false;
+
+  public categoriaV: any[] = [{ "categoria": "Educacion" },
+  { "categoria": "Ciencia" },
+  { "categoria": "Tecnologia" },
+  { "categoria": "Arte" },
+  { "categoria": "Cultura" },
+  { "categoria": "Gastronomia" },
+  { "categoria": "Medicina" },
+  { "categoria": "Otro" },
+  ];
 
 
 
   @ViewChild('NAV') nav: Nav;
 
   constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
-  
+
   }
 
 
@@ -50,26 +65,56 @@ export class NuevaOfertaPage {
   }*/
 
 
-  activarOtro()
-  {
-  
-  if(this.Categoria=="Otro")
-  {
-    this.bander=true;
-  }else
-  {
-    this.bander=false;
-  }
+  activarOtro() {
+
+    if (this.Categoria == "Otro") {
+      this.bander = true;
+    } else {
+      this.bander = false;
+    }
   }
 
+
+  // funcion para activar botones
+  activarBotonesredesSociales(btnF, btnI, btnT) {
+    if (btnF == '1') {
+      this.btnFaceboockAct = !this.btnFaceboockAct;
+      this.btnFaceboock = !this.btnFaceboock;
+    }
+
+    if (btnI == '1') {
+      this.btnInstagramAct = !this.btnInstagramAct;
+     this.btnInstagram = !this.btnInstagram;
+
+    }
+
+    if(btnT=='1')
+    {
+     this.btnTwiterAct =  !this.btnTwiterAct;
+     this.btnTwiter = !this.btnTwiter;
+
+    }
+
+  }
   public toggleTextPassword(): void {
     this.isActiveToggleTextPassword = (this.isActiveToggleTextPassword == true) ? false : true;
+
+    if(this.isActiveToggleTextPassword)
+      {
+        this.banderOtro=true;
+      }else
+      {
+        this.banderOtro=false;
+      }
   }
+
+
+
 
 
   goToPublicistaPage() {
     this.navCtrl.push(PublicistaPage);
   }
 
-  
+
 }
