be3fd18991d224ecc67ee7fd88e2ad5970f8d418
vanessitaf8 <caryto_z@hotmail.com>
2019-01-16 00:28:56 -0500

    final Arreglo nueva solicitud

--- kansappAdministrador1/src/pages/list/list.ts
+++ kansappAdministrador1/src/pages/list/list.ts
@@ -12,16 +12,47 @@ export class ListPage {
   icons: string[];
   items: Array<{ title: string, note: string, icon: string }>;
 
-  public  vectorMyOfertas: any;
-   public banderNewOffert: any;
-   public banderMyOffert: any;
-   public vectorOfertas: any;
-   public arrayDeCadenas: any;
-  constructor(public _administradorService:AdministradorService, public _solicitudesService: SolicitudesService, public navCtrl: NavController, public navParams: NavParams) {
+  public vectorMyOfertas: any;
+  public banderNewOffert: any;
+  public banderMyOffert: any;
+  public vectorOfertas: any;
+  public arrayDeCadenas: any;
+
+  public Categoria;
+
+  public objUpdateOferta = {
+    estado:null,
+    Links_to_work: null,
+    Categoria: null,
+    OtraCategoria: null,
+    Faceboock: null,
+    Instagram: null,
+    Twiter: null,
+    OtraRed: null,
+    Tiempo: null,
+    Precio: null,
+    Alcance: null,
+    Inf_extra: null,
+    emitter:null
+  };
+
+  
+  constructor(public _administradorService: AdministradorService, public _solicitudesService: SolicitudesService, public navCtrl: NavController, public navParams: NavParams) {
+
+
+
 
   }
 
+  activarOtro() {
 
+    if (this.Categoria == "Otro") {
+      //this.bander = true;
+    } else {
+      //this.bander = false;
+      console.log(this.Categoria);
+    }
+  }
 
   NuevasSolicitudes() {
     this.vectorMyOfertas = null;
@@ -33,17 +64,33 @@ export class ListPage {
       console.log("esto iene de la peticion" + JSON.stringify(response));
       if (response.messagess[0] != undefined) {
         this.vectorOfertas = response.messagess;
-       
-       // console.log(arrayDeCadenas);
-        //this.darvuelta();
+
         console.log("trayendo solicitudes de viajes", this.vectorOfertas);
-        //localStorage.setItem("vectorViajesMios", JSON.stringify(this.vectorViajes));
-        
-       
+
       }
     }, (err) => { console.log("Existen Complicaciones Intente mas tarde", err) }
     );
   }
+
+  AceptarOfertaNueva(Vector,estado)
+  {
+
+console.log("vector ofertas deve venir al que di click"+JSON.stringify(Vector));
+this.objUpdateOferta.estado=estado;
+this.objUpdateOferta.Links_to_work=Vector.Links_to_work;
+this.objUpdateOferta.Categoria =this.Categoria;
+this.objUpdateOferta.Inf_extra=Vector.Inf_extra;
+this.objUpdateOferta.emitter=Vector.emitter._id;
+   
+
+   
+  
+    
+  }
+
+ 
+  
+
 }
 
 
